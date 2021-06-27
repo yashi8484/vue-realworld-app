e852881a1057442ac0511a021a55655756930ee9
yashi8484 <kobayashi@giftee.co>
2021-06-27 18:26:53 +0900

    Display the API response in the article list

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -21,48 +21,7 @@
             </ul>
           </div>
 
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href="profile.html"
-                ><img src="http://i.imgur.com/Qr71crq.jpg"
-              /></a>
-              <div class="info">
-                <a href="" class="author">Eric Simons</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 29
-              </button>
-            </div>
-            <a href="" class="preview-link">
-              <h1>How to build webapps that scale</h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
-
-          <div class="article-preview">
-            <div class="article-meta">
-              <a href="profile.html"
-                ><img src="http://i.imgur.com/N4VcUeJ.jpg"
-              /></a>
-              <div class="info">
-                <a href="" class="author">Albert Pai</a>
-                <span class="date">January 20th</span>
-              </div>
-              <button class="btn btn-outline-primary btn-sm pull-xs-right">
-                <i class="ion-heart"></i> 32
-              </button>
-            </div>
-            <a href="" class="preview-link">
-              <h1>
-                The song you won't ever stop singing. No matter how hard you
-                try.
-              </h1>
-              <p>This is the description for the post.</p>
-              <span>Read more...</span>
-            </a>
-          </div>
+          <Article v-for="article in articles" :article="article" />
         </div>
 
         <div class="col-md-3">
@@ -85,5 +44,33 @@
     </div>
   </div>
 </template>
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import { ref } from "vue";
+import { ArticleEdge } from "../../types";
+import { getArticles } from "../../utils/restClient";
+import Article from "./home/Article.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: { Article },
+  setup: () => {
+    const articles = ref<ArticleEdge['articles'] | undefined>(undefined);
+
+    (async () => {
+      const response = await getArticles();
+
+      if (response.status === "error") {
+        articles.value = undefined;
+        return;
+      }
+      articles.value = response.data.articles;
+    })();
+
+    return {
+      articles,
+    };
+  },
+});
+</script>
 <style scoped></style>
---
+++ src/components/pages/home/Article.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="article-preview">
+    <div class="article-meta">
+      <a href="profile.html">
+        <img :src="article.author.image" :alt="article.author.username" />
+      </a>
+      <div class="info">
+        <a href="" class="author">{{ article.author.username }}</a>
+        <span class="date">{{ article.updatedAt.toString() }}</span>
+      </div>
+      <button class="btn btn-outline-primary btn-sm pull-xs-right">
+        <i class="ion-heart"></i> {{ article.favoritesCount }}
+      </button>
+    </div>
+    <a href="" class="preview-link">
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.description }}</p>
+      <span>Read more...</span>
+    </a>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import type { PropType } from "vue";
+import type { Article } from "../../../types";
+
+export default defineComponent({
+  name: "Article",
+  props: {
+    article: {
+      type: Object as PropType<Article>,
+      required: true,
+    },
+  },
+});
+</script>
+<style scoped></style>
